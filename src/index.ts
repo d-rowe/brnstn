02c43fc5812ce264f1d50c3db63c182ea7078278
Daniel Rowe <danielrowetech@gmail.com>
2021-01-09 01:21:33 -0800

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export {default as Chord, MidiChord} from './Chord';
+export {default as Chord, IntegerChord} from './Chord';
 export {default as Interval} from './Interval';
 export {default as Pitch} from './Pitch';
 export {default as Helpers} from './Helpers';
